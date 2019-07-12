1d5df035f7ba8a495cd13e03fc08f561d73dd5bd
Warren Ayling <warren.ayling@wozitech-ltd.co.uk>
2019-07-12 14:08:09 +0100

    Introducing ASCWDS JWT generation and validation

--- utils/registrationApprovals.js
+++ utils/registrationApprovals.js
@@ -1,12 +1,65 @@
+const JWT = require('../utils/security/JWT');
+const axios = require('axios');
+
 const approveReject = async (request) => {
   if (request.actions && Array.isArray(request.actions)) {
+    const backendApiJWT = JWT.ASCWDS_JWT(15, request.user.name);
+
     if (request.actions[0] && request.actions[0].value === 'accept') {
-      console.log("ACCEPTING THE REGISTRATION");
-      return true;
+
+      try {
+        const apiResponse = await axios.get(
+          `${apiUrl}/${establishmentUID}/approve`,
+          {
+            params: null,
+            headers: {
+              Authorization: `Bearer ${backendApiJWT}`
+          }
+        });
+
+        if (apiResponse.status === 200) {
+          console.log("Accepting the registration");
+          return true;
+        } else {
+          console.log("The ASC WDS failed to accept registration");
+
+        }
+      } catch (err) {
+        console.error("WA TODO - Need better error handling");
+        return false;
+      }
+
     } else {
       const reasonForRejection = request.actions[0].selected_options;
       console.log("REJECTING THE REGISTRATION: ", reasonForRejection[0].value);
-      return false;
+
+
+      try {
+        const apiResponse = await axios.get(
+          `${apiUrl}/${establishmentUID}/reject`,
+          {
+            params: {
+              reject: {
+                reason: reasonForRejection[0].value,
+                notes: '',    // this will come later
+              }
+            },
+            headers: {
+              Authorization: `Bearer ${backendApiJWT}`
+          }
+        });
+
+        if (apiResponse.status === 200) {
+          console.log("Rejecting the registration");
+          return true;
+        } else {
+          console.log("The ASC WDS failed to reject registration");
+          return false;
+        }
+      } catch (err) {
+        console.error("WA TODO - Need better error handling");
+        return false;
+      }
     }
   } else {
     return null;
---
+++ utils/security/JWT.js
@@ -0,0 +1,51 @@
+const config = require('../../config/config');
+const jwt = require('jsonwebtoken');
+const Token_Secret = process.env.Token_Secret;
+
+const THIS_ISS = config.get('jwt.iss');
+
+// this generates the JWT that can be presented to the the ASC WDS Backend
+exports.ASCWDS_JWT = (ttlSeconds, username) => {
+  var claims = {
+    sub: username,
+    aud: config.get('jwt.aud.internalAdminApp'),
+    iss: THIS_ISS
+  };
+
+  return jwt.sign(JSON.parse(JSON.stringify(claims)), Token_Secret, {expiresIn: `${ttlSeconds}s`});
+};
+
+
+
+// this generates the JWT that can be presented to the the ASC WDS Backend
+exports.ASCWDS_JWT = (ttlSeconds, username) => {
+  var claims = {
+    sub: username,
+    aud: config.get('jwt.aud.internalAdminApp'),
+    iss: config.get('jwt.iss')
+  };
+
+  return jwt.sign(JSON.parse(JSON.stringify(claims)), Token_Secret, {expiresIn: `${ttlSeconds}s`});
+};
+
+// middleware for confirming ASC Internal Admin JWT 
+exports.isAdmin = (req, res , next) => {
+  const token = getToken(req.headers[AUTH_HEADER]);
+
+  if (token) {
+    jwt.verify(token, Token_Secret, function (err, claim) {
+      if (err || claim.aud !== config.get('jwt.aud.internalAdminApp') || claim.iss !== THIS_ISS) {
+        return res.status(403).send('Invalid Token');
+      } else {
+        if (claim.role !== 'Admin') {
+          return res.status(403).send('You\'re not known to Internal Admin');
+        } else {
+          next();
+        }
+      }
+    });
+  } else {
+    // not authenticated
+    res.status(401).send('Requires authorisation');
+  }
+};
