f89bdaf42bbe2cead46cf9109abfe12c175069cd
Warren Ayling <warren.ayling@wozitech-ltd.co.uk>
2019-07-12 17:41:53 +0100

    Callback on approve/reject to the Backend API with JWT token authentication

--- utils/registrationApprovals.js
+++ utils/registrationApprovals.js
@@ -1,32 +1,36 @@
-const JWT = require('../utils/security/JWT');
 const axios = require('axios');
+const JWT = require('../utils/security/JWT');
+const config = require('../config/config');
 
-const approveReject = async (request) => {
+const apiUrl = config.get('app.url') + '/api/establishment';
+
+const approveReject = async (establishmentUID, request) => {
   if (request.actions && Array.isArray(request.actions)) {
     const backendApiJWT = JWT.ASCWDS_JWT(15, request.user.name);
 
     if (request.actions[0] && request.actions[0].value === 'accept') {
 
       try {
-        const apiResponse = await axios.get(
-          `${apiUrl}/${establishmentUID}/approve`,
+        const apiResponse = await axios(
           {
-            params: null,
+            method: 'post',
+            url: `${apiUrl}/${establishmentUID}/approve`,
             headers: {
               Authorization: `Bearer ${backendApiJWT}`
+            },
           }
-        });
+        );
 
         if (apiResponse.status === 200) {
           console.log("Accepting the registration");
           return true;
         } else {
           console.log("The ASC WDS failed to accept registration");
-
+          return null;
         }
       } catch (err) {
-        console.error("WA TODO - Need better error handling");
-        return false;
+        console.error("WA TODO - Need better error handling: ", err);
+        return null;
       }
 
     } else {
@@ -35,30 +39,32 @@ const approveReject = async (request) => {
 
 
       try {
-        const apiResponse = await axios.get(
-          `${apiUrl}/${establishmentUID}/reject`,
+        const apiResponse = await axios(
           {
-            params: {
+            method: 'post',
+            url: `${apiUrl}/${establishmentUID}/reject`,
+            headers: {
+              Authorization: `Bearer ${backendApiJWT}`
+            },
+            data: {
               reject: {
                 reason: reasonForRejection[0].value,
                 notes: '',    // this will come later
               }
-            },
-            headers: {
-              Authorization: `Bearer ${backendApiJWT}`
+            }
           }
-        });
+        );
 
         if (apiResponse.status === 200) {
           console.log("Rejecting the registration");
-          return true;
+          return false;
         } else {
           console.log("The ASC WDS failed to reject registration");
-          return false;
+          return null;
         }
       } catch (err) {
-        console.error("WA TODO - Need better error handling");
-        return false;
+        console.error("WA TODO - Need better error handling: ", err);
+        return null;
       }
     }
   } else {
