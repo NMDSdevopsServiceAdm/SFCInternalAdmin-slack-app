34f97e0f00806dff47346db68cb8991467b857c8
EC2 Default User <ec2-user@ip-10-0-0-32.eu-west-1.compute.internal>
2019-07-14 08:45:32 +0000

    Removing unnecessary console logging

--- utils/verifySignature.js
+++ utils/verifySignature.js
@@ -4,8 +4,6 @@ const crypto = require('crypto');
 const timingSafeCompare = require('tsscmp');
 
 const isVerified = (req) => { 
-  console.log("WA DEBUG - slack secret: ", config.get('slack.secret'))
-
   const signature = req.headers['x-slack-signature'];
   const timestamp = req.headers['x-slack-request-timestamp'];
   const hmac = crypto.createHmac('sha256', config.get('slack.secret'));
@@ -15,16 +13,10 @@ const isVerified = (req) => {
   const fiveMinutesAgo = ~~(Date.now() / 1000) - (60 * 5);
   if (timestamp < fiveMinutesAgo) return false;
 
-  console.log("WA DEBUG - got this far")
-
   hmac.update(`${version}:${timestamp}:${req.rawBody}`);
-
-//  console.log(hmac.digest('hex'));
-//  console.log(hash);
 
   // check that the request signature matches expected value
   const hashCheck = timingSafeCompare(hmac.digest('hex'), hash);
-  console.log("WA DEBUG - hash check: ", hashCheck)
   return hashCheck;
 }; 
   
